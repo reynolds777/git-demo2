9bf05a225c230f16854e8723a6b9aa474aceefb2
Reys <1814982550@qq.com>
2022-10-12 20:20:37 +0800

    commit from IDEA ---hotfix 2.2

--- src/main/java/com/reynolds/git/GitDemo.java
+++ src/main/java/com/reynolds/git/GitDemo.java
@@ -4,6 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
-        System.out.println("hello hotfix");
+        System.out.println("hello master");
     }
 }
