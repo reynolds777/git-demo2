bc64e8e3b82a9c47d34c75edf66197227a3ecf37
Reys <1814982550@qq.com>
2022-10-12 20:24:28 +0800

    commit from IDEA ---version 2.2

--- src/main/java/com/reynolds/git/GitDemo.java
+++ src/main/java/com/reynolds/git/GitDemo.java
@@ -5,5 +5,6 @@ public class GitDemo {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello master");
+        System.out.println("hello master");
     }
 }
